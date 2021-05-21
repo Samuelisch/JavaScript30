acb64396d88679f4ffa7b11dd449a33532eb6bd8
Samuelisch <samuelchanwk@hotmail.com>
2021-05-21 16:49:17 +0800

    redo for understanding of selectors

--- 01 - JavaScript Drum Kit/js/drumkit.js
+++ 01 - JavaScript Drum Kit/js/drumkit.js
@@ -1,16 +1,15 @@
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`)
     if (!audio) return;
-    audio.currentTime = 0;
+
     audio.play();
     key.classList.add('playing');
-}
+    audio.currentTime = 0;
+};
 
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
+    if (e.propertyName != "transform") return;
     this.classList.remove('playing');
 }
 
