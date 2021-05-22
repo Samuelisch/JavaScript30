edd36bfbf91dc7a8645e96a97eb2acf5fd864475
Samuelisch <samuelchanwk@hotmail.com>
2021-05-22 23:51:19 +0800

    redo JS30 CSS + JS clock

--- 02 - JS and CSS Clock/js/style.js
+++ 02 - JS and CSS Clock/js/style.js
@@ -5,29 +5,15 @@ const time = document.querySelector('.time');
 
 function setDate() {
     const now = new Date();
-
-    const zero = (x) => {
-        if (x < 10) {
-            return `0${x}`;
-        }
-        return x;
-    }
-
-    const seconds = zero(now.getSeconds());
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-
-    const mins = zero(now.getMinutes());
-    const minsDegrees = ((mins / 60) * 360) + 90;
-    minHand.style.transform = `rotate(${minsDegrees}deg)`
-
-    const hour = zero(now.getHours());
-    const hourDegrees = ((hour / 12) * 360) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-    time.textContent = `${hour} : ${mins} : ${seconds}`;
+    const seconds = now.getSeconds();
+    const secondsDegrees = (seconds / 60) * 360 + 90;
+    const minutes = now.getMinutes();
+    const minutesDegrees = (minutes / 60) * 360 + 90;
+    const hours = now.getHours();
+    const hoursDegrees = (hours / 12) * 360 + 90
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
-
-//understand transition all, and how to disable then enable it.
